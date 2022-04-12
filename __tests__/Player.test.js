448726b4692b44f8d7ba71a3f0fb509913612889
Jason Arritt <jsn.arritt@gmail.com>
2022-04-12 12:52:56 -0400

    Get stats and get inventory functions

---
+++ __tests__/Player.test.js
@@ -0,0 +1,40 @@
+
+const Potion = require('../lib/Potion.js');
+jest.mock('../lib/Potion.js');
+
+const {jest: requiredJest } = require('@jest/globals');
+const Player = require('../lib/Player.js');
+
+console.log(new Potion());
+
+test('creates a player object', () => {
+    const player = new Player('Jason');
+
+    expect(player.name).toBe('Jason');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    )
+})
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Jason');
+  
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+  });
+
+  test('gets inventory from player or returns false', () => {
+    const player = new Player('Jason');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+  });
